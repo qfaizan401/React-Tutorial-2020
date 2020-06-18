5e9a37403b0f33f8d7f0d61666ddb095fa4435e1
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-19 02:15:58 +0500

    Update index.js

--- my_first_app/src/index.js
+++ my_first_app/src/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function Hi() {
-  return <div>Hello world</div>
+  return <div>
+      Hello <strong>Faizan</strong>
+      </div>
 }
 
 ReactDOM.render (
