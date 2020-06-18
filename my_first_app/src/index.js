d130744bef4d1d9722740b197e0c85f2b836fd7d
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-19 02:09:31 +0500

    added index.js

---
+++ my_first_app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Hi() {
+  return <div>Hello world</div>
+}
+
+ReactDOM.render (
+    <Hi/>,
+    document.querySelector('#root')
+)
