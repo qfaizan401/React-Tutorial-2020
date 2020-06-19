56056eaa759f30f0104a271e8a54aff8fc43683f
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-20 03:16:39 +0500

    styled head and foot

--- todo_app/src/components/Footer/Footer.js
+++ todo_app/src/components/Footer/Footer.js
@@ -5,9 +5,6 @@ function Footer () {
     return(
         <div className = { styles.foot }>
             <footer>Created by Faizan Qureshi</footer>
-            <div className = { styles.profile }>
-                <a href = 'https://github.com/qfaizan401'>My Git Profile</a>
-            </div>
         </div>
     )
 }
