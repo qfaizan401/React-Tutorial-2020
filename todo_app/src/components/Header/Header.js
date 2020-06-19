9a3df0ba7a3ec6c0f9b2a49d379e5c6674715028
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-20 02:11:18 +0500

    created and styled Header

--- todo_app/src/components/Header/Header.js
+++ todo_app/src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Header.module.css'
 
 function Header () {
     return(
         <div>
-            <header>Todo App</header>
+            <header className = {styles.head}>Todo App</header>
         </div>
     )
 }
