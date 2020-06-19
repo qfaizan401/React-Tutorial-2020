f51a215e106a81fadcf54f2e65812ed9a649dc58
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-19 18:17:27 +0500

    added more components

---
+++ my_first_app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer () {
+    return(
+        <div>
+            <h3>This is My Footer Component</h3>
+            <a href = 'https://github.com/qfaizan401'>My Git Profile</a>
+        </div>
+    )
+}
+
+export default Footer
---
+++ my_first_app/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header () {
+    return(
+        <div>
+            <h1>This is my Header Compotnent</h1>
+            <nav>
+                <li>Home</li>
+                <li>About</li>
+                <li>Links</li>
+                <li>Contect</li>
+            </nav>
+        </div>
+    )
+}
+ export default Header
