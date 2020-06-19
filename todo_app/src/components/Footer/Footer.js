a2089466b9a2b42e2c0e66a90b9bf38c182cb13c
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-20 03:34:01 +0500

    Update Footer.js

--- todo_app/src/components/Footer/Footer.js
+++ todo_app/src/components/Footer/Footer.js
@@ -2,9 +2,11 @@ import React from 'react'
 import styles from './Footer.module.css'
 
 function Footer () {
+    const FirstName = "Faizan"
+    const LastName = "Qureshi"
     return(
         <div className = { styles.foot }>
-            <footer>Created by Faizan Qureshi</footer>
+            <footer>Created by {FirstName + " " +LastName}</footer>
         </div>
     )
 }
