fb482dd15b844d91ab077d132094254a9eb00531
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-20 01:35:34 +0500

    update componets

--- todo_app/src/components/index.js
+++ todo_app/src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Header } from './Header'
-export { default as MainContent } from './MainContent'
-export { default as Footer } from './Footer'
+export { default as Header } from './Header/Header'
+export { default as MainContent } from './MainContent/MainContent'
+export { default as Footer } from './Footer/Footer'
