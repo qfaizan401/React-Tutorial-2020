b44112d99004f422d4d7f1a986b14d7f0cbcccba
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-19 18:24:18 +0500

    Update App.js

--- my_first_app/src/App.js
+++ my_first_app/src/App.js
@@ -1,2 +1,16 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import MyInfo from './components/MyInfo'
 
+function App () {
+    return(
+        <div>
+            <Header />
+            <MyInfo />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
