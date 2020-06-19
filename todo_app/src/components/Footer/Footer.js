d62f462b12df557f3b52c319f43706ff80d729fc
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-20 02:37:00 +0500

    added some styles

--- todo_app/src/components/Footer/Footer.js
+++ todo_app/src/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import styles from './Footer.module.css'
 
 function Footer () {
     return(
-        <div>
+        <div className = { styles.foot }>
             <footer>Created by Faizan Qureshi</footer>
-            <a href = 'https://github.com/qfaizan401'>My Git Profile</a>
+            <div className = { styles.profile }>
+                <a href = 'https://github.com/qfaizan401'>My Git Profile</a>
+            </div>
         </div>
     )
 }
