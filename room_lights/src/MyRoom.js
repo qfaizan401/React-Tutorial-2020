2a6d33c0a44cc04524b80f22a4e5e5201933e612
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-24 01:52:46 +0500

    completed

--- room_lights/src/MyRoom.js
+++ room_lights/src/MyRoom.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
+import './MyRoom.css'
 
 const MyRoom = () => {
     let [Light, setLight] = useState(true)
+    let [Temp, setTemp] = useState(22)
 
     return(
-        <div>
-            LIGHT: { Light? "On":"Off" }
-            <br/>
-            <button onClick={() => setLight(Light = true)}>ON</button>
-            <button onClick={() => setLight(Light = false)}>OFF</button>
+        <div className={`room ${ Light? 'on':'off' }`}>
+            <div>
+                LIGHTS: { Light? "On":"Off" }
+                <br/>
+                <button onClick={() => setLight(Light = true)}>ON</button>
+                <button onClick={() => setLight(Light = false)}>OFF</button>
+            </div>
+            <div>
+                TEMPERATURE: {Temp}
+                <br/>
+                <button onClick={() => setTemp(Temp+1)}>+</button>
+                <button onClick={() => setTemp(Temp-1)}>-</button>
+                <button onClick={() => setTemp(Temp=22)}>RESET</button>
+            </div>
         </div>
     )
 }
