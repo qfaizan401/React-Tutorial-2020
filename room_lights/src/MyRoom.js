3d00f6eed8a51abc4facd8b4d7478e3df2a8d7d8
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-06-24 01:37:11 +0500

    Update MyRoom.js

--- room_lights/src/MyRoom.js
+++ room_lights/src/MyRoom.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MyRoom = () => {
+    let [Light, setLight] = useState(true)
+
     return(
         <div>
-            Hello from room component
+            LIGHT: { Light? "On":"Off" }
+            <br/>
+            <button onClick={() => setLight(Light = true)}>ON</button>
+            <button onClick={() => setLight(Light = false)}>OFF</button>
         </div>
     )
 }
